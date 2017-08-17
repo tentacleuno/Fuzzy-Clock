6c1a1fdd276d970a58a0638977da04738adba130
Cj Malone <CjMalone@mail.com>
2017-08-17 10:08:44 +0100

    Convert to Fuzzy Clock

--- extension.js
+++ extension.js
@@ -1,82 +1,50 @@
 const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
 
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const Convenience = Me.imports.convenience;
+var HOURS = [ "ONE",
+    "TWO",
+    "THREE",
+    "FOUR",
+    "FIVE",
+    "SIX",
+    "SEVEN",
+    "EIGHT",
+    "NINE",
+    "TEN",
+    "ELEVEN",
+    "TWELVE" ];
 
-var settings;
-
-function init() {
-    settings = Convenience.getSettings();
-}
-
-var fromCodePoint = function() {
-    var MAX_SIZE = 0x4000;
-    var codeUnits = [];
-    var highSurrogate;
-    var lowSurrogate;
-    var index = -1;
-    var length = arguments.length;
-    if (!length) { return ''; }
-    var result = '';
-    while (++index < length) {
-        var codePoint = Number(arguments[index]);
-        if (
-            !isFinite(codePoint) ||       // `NaN`, `+Infinity`, or `-Infinity`
-            codePoint < 0 ||              // not a valid Unicode code point
-            codePoint > 0x10FFFF ||       // not a valid Unicode code point
-            Math.floor(codePoint) != codePoint // not an integer
-        ) {
-            throw RangeError('Invalid code point: ' + codePoint);
-        }
-        if (codePoint <= 0xFFFF) { // BMP code point
-            codeUnits.push(codePoint);
-        } else { // Astral code point; split in surrogate halves
-            // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
-            codePoint -= 0x10000;
-            highSurrogate = (codePoint >> 10) + 0xD800;
-            lowSurrogate = (codePoint % 0x400) + 0xDC00;
-            codeUnits.push(highSurrogate, lowSurrogate);
-        }
-        if (index + 1 == length || codeUnits.length > MAX_SIZE) {
-            result += String.fromCharCode.apply(null, codeUnits);
-            codeUnits.length = 0;
-        }
-    }
-    return result;
-};
+var FUZZY_RULES = [ "%s O'CLOCK",
+    "FIVE past %s",
+    "TEN past %s",
+    "QUARTER past %s",
+    "TWENTY past %s",
+    "TWENTY FIVE past %s",
+    "HALF past %s",
+    "TWENTY FIVE to %s",
+    "TWENTY to %s",
+    "QUARTER to %s",
+    "TEN to %s",
+    "FIVE to %s" ];
 
 function overrider(lbl) {
     var t = lbl.get_text();
-    var FORMAT = settings.get_string("override-string");
-    var desired = FORMAT;
-    if (FORMAT.indexOf("%") > -1) {
-        var now = GLib.DateTime.new_now_local();
-        if (FORMAT.indexOf("%f") > -1) {
-            var hour = now.get_hour();
-            // convert from 0-23 to 1-12
-            if (hour > 12) {
-                hour -= 12;
-            }
-            if (hour == 0) {
-                hour = 12;
-            }
-            var clockFaceCodePoint = 0x1f550 + (hour - 1);
-            var minute = now.get_minute();
-            if (minute >= 30) {
-                clockFaceCodePoint += 12;
-            }
-            var repl;
-            if (String.fromCodePoint) {
-                repl = String.fromCodePoint(clockFaceCodePoint)
-            } else {
-                repl = fromCodePoint(clockFaceCodePoint);
-            }
-            desired = desired.replace("%f", repl);
-        }
-        desired = now.format(desired);
-    }
+
+    var now = GLib.DateTime.new_now_local();
+    var hour = now.get_hour();
+    var minute = now.get_minute();
+    minute = Math.floor((minute + 2) / 5);
+
+    if (hour >= 12)
+        hour -= 12;
+
+    hour -= 1;
+
+    if (minute > 6)
+        hour += 1;
+
+    var desired = FUZZY_RULES[minute].format(HOURS[hour]);
+
     if (t != desired) {
         last = t;
         lbl.set_text(desired);
